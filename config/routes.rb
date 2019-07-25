f676803c91d4856f9331f44b35f8b4fb6443fe83
Brian J. Fox <bfox@opuslogica.com>
2019-07-25 12:53:40 -0700

    Include generic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,28 @@
-Rails.application.routes.draw do
+Openopus::Core::Api::Engine.routes.draw do
+  namespace :openopus:core:api, :path => "/",defaults: { format: 'json' } do
+    match '*path' => "base#options", :via => [:options]
+  
+    namespace "authentication" do 
+      get 'facebook' 
+      post 'facebook'
+      get 'login'
+      post 'login'
+      get 'signup'
+      post 'signup'
+      get 'logout'
+      post 'logout'
+      post 'change_password'
+      post 'reset_password'
+      post 'recover_password'
+    end
+
+    get    'version'                    => 'misc#version'
+    get    'whoami'                     => 'misc#whoami'
+
+    get    '(:namespace/):model/:id'    => 'rest#read',    constraints: { id: /(\h|-)*/, model: /\D+/ }
+    match  '(:namespace/):model/:id'    => 'rest#update',  constraints: { id: /(\h|-)*/, model: /\D+/ }, via: [:post, :put, :patch]
+    delete '(:namespace/):model(/:id)'  => 'rest#destroy', constraints: { id: /(\h|-)*/, model: /\D+/ }
+    get    '(:namespace/):model'        => 'rest#index',   constraints: { id: /(\h|-)*/, model: /\D+/ }
+    post   '(:namespace/):model'        => 'rest#create',  constraints: { id: /(\h|-)*/, model: /\D+/ }
+  end
 end
